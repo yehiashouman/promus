d033b8fe0f9b184c8f9a0e5b42749f80e3ee2096
yehiashouman <yehia.shouman@gmail.com>
2015-11-30 03:23:54 +0200

    ultrasonic sensor hc so4 implementation

--- promus/sensor/Ultrasonic_HCSR04.py
+++ promus/sensor/Ultrasonic_HCSR04.py
@@ -1,5 +1,6 @@
 import Base promus.core.Base
 import promus.core.events
+import time
 class Ultrasonic_HCSR04(events.EventDispatcher):
     def __init__(trigPin,echoPin,gpio):
         self.trigPin = trigPin
@@ -7,10 +8,22 @@ class Ultrasonic_HCSR04(events.EventDispatcher):
         self.__gpio = gpio
         self.__gpio.setup(self.trigPin, self.__gpio.OUT)
         self.__gpio.setup(self.echoPin, self.__gpio.IN)
+        self.__gpio.output(self.trigPin, self.__gpio.LOW)
+        self.__signalon  = 0
+        self.__signaloff = 0
+        time.sleep(0.3)
         return;
     def trigger():
-
-        """" detect event here
-        """"
-        self.dispatchEvent(events.Event(events.Event.OBJECT_DETECTED))
+        self.__gpio.output(self.trigPin, self.__gpio.HIGH)
+        time.sleep(0.00001)
+        self.__gpio.output(self.trigPin, self.__gpio.LOW)
+        while self.__gpio.input(self.echoPin) == 0:
+            self.__signaloff = time.time()
+        while self.__gpio.input(self.echoPin) == 1:
+            self.__signalon = time.time()
+        timepassed = self.__signalon - self.__signaloff
+        distance = timepassed * 17250
+        evt = events.Event(events.Event.OBJECT_DETECTED)
+        evt.distance = distance
+        self.dispatchEvent(evt)
         return;
--- tutorials/test.py
+++ tutorials/test.py
@@ -6,15 +6,17 @@ import Environment from promus.system.Environmente
 
 env = Environment(GPIO, GPIO.BCM)
 myUltraSonicSensor = Ultrasonic_HCSR04(18,26,GPIO)
-myUltraSonicSensor.addEventListener(Event.OBJECT_DETECTED,handler)
+
+def ultraSonicHandler(evt):
+{
+    print evt.distance
+    myGreenLed.on()
+
+}
+myUltraSonicSensor.addEventListener(Event.OBJECT_DETECTED,ultraSonicHandler)
 myGreenLed = SingleColorLed(23,GPIO)
 
 env.addObject(myGreenLed)
 env.addObject(myUltraSonicSensor)
 
-
-def handler():
-{
-    myGreenLed.on()
-
-}
+myUltraSonicSensor.trigger()
